6c9b082ed7b0754153ffb3bbccf80f03a8ddd7e5
Harry Pierson <harrypierson@ngd.neo.org>
2020-08-27 14:30:38 -0700

    fix mint operation

--- contract/Apoc.cs
+++ contract/Apoc.cs
@@ -18,8 +18,17 @@ namespace ApocSample
         static readonly byte[] Owner = "Nc2TJmEh7oM2wrXKdAQH5gHpy8HnyztcME".ToScriptHash();
         static readonly ulong TokensPerNEO = 1_000_000_000;
         static readonly ulong TokensPerGAS = 1;
-        static readonly byte[] NeoToken = "0xde5f57d430d3dece511cf975a8d37848cb9e0525".HexToBytes();
-        static readonly byte[] GasToken = "0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc".HexToBytes();
+
+        // TODO: Preview 3 doesn't provide a way to convert hex string to script hash correctly
+        //       https://github.com/neo-project/neo-devpack-dotnet/issues/342
+        //       temporary workaround to manually reverse the hex representation.
+
+        // static readonly byte[] NeoToken = "0xde5f57d430d3dece511cf975a8d37848cb9e0525".HexToBytes();
+        // static readonly byte[] GasToken = "0x668e0c1f9d7b70a99dd9e06eadd4c784d641afbc".HexToBytes();
+
+        static readonly byte[] NeoToken = "0x25059ecb4878d3a875f91c51ceded330d4575fde".HexToBytes();
+        static readonly byte[] GasToken = "0xbcaf41d684c7d4ad6ee0d99da9707b9d1f0c8e66".HexToBytes();
+        
         #endregion
 
         #region Notifications
